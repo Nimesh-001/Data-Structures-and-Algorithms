80e907d16e1488a18177878b316a01fa1ab15ffc
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-06-19 00:22:16 +0530

    Create ex2.c

---
+++ Link list/ex2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+	int data;
+	struct node* next;
+};
+
+struct node* head = null;
+
+void insertatbegging(int value){
+	struct node* new node =(struct node*)malloc(size of(struct node));
+	new node -> data = value;
+	new node -> next = head;
+	head = new node;
+}
+
+void insertatend(int value){
+	struct node* new node =(struct node*)malloc(size of(struct node));
+	new node -> data =value;
+	new node -> next =null;
+	
+}
+
+void delete(int value){
+	
+}
+
+void display(){
+	
+}
+
+int search(int value){
+	
+	return 0;
+}
+
+int main(){
+	
+	
+	return 0;
+}
