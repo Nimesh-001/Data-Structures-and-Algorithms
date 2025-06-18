12797fc873215f015d433408fc24b2d6bd367ce7
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-06-19 00:13:27 +0530

    Create ex1.c

---
+++ Link list/ex1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+	int data;
+	struct node* next;
+};
+
+struct node* head = null;
+
+void insertatbegging(int value){
+	struct node* new node =(struct node*)malloc(size of(struct node*));
+	new node -> data = value;
+	new node -> next = head;
+	head = null;
+}
+
+void insertatend(int value){
+	
+}
+
+void delete(int value){
+	
+}
+
+void display(){
+	
+}
+
+int search(int value){
+	
+	return 0;
+}
+
+int main(){
+	
+	
+	return 0;
+}
